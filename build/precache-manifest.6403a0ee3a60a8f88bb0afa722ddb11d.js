e0fcfb47c66c08e34dc955a3b5b1252f6e5beecf
Surukati Asutosh Dora <78288601+SurukatiAsutoshDora@users.noreply.github.com>
2022-06-29 19:08:40 +0530

    Add files via upload

---
+++ build/precache-manifest.6403a0ee3a60a8f88bb0afa722ddb11d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9895585f84fde954e1399de9679002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "bbe0a55dde61a7440039",
+    "url": "/static/js/main.f251714f.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
